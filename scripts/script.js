562837f6b0e6a93223ad94996da563248577f973
Evgeny Kryzhu <e.kryzhu@colynn.pro>
2025-01-30 21:43:48 +0300

    cw 7

--- scripts/script.js
+++ scripts/script.js
@@ -1,82 +1,112 @@
-let str = 'Lorem ipsum dolor, sit amet consectetur\nadipisicing elit. Provident consectetur temporibus ea rerum cumque repudiandae.';
+const validatePhone = (number) => {
+  const numberRegExp = /^\+?[0-9]{1,3}\([0-9]{2}\)\d\d\d-?\d\d-?\d\d$/g
+  return numberRegExp.test(number)
+}
 
-console.log(str.split(' '));
+// const phone = prompt('Введите номер телефона')
+// console.log(validatePhone(phone))
 
-/**
- * @typedef {Object} FunctionParams
- * @property {string} string
- * @property {number} test
- */
+let date = 'Lorem ipsum dolor, sit amet consectetur 2007.01.01 adipisicing elit. Possimus, vero.'
+let regDate = /(\d{4})\.(\d{2})\.(\d{2})/i
+console.log(date.replace(regDate, '$3/$2/$1'));
+console.log(regDate.exec(date));
+console.log(date.match(regDate));
 
-/**
- * @param {string} string 
- * @returns {number}
- */
-const strCount = (string) => {
-  let count = 0;
-  for (let i = 0; i < string.length; i++) {
-    if (string[i] === ' ' || string[i] === '\n') {
-      count++
+const addZero = (time) => time < 10 ? `0${time}` : time
+
+
+const timer = () => {
+  let dateNow = new Date()
+  console.clear()
+  console.log(`${addZero(dateNow.getHours())}:${addZero(dateNow.getMinutes())}:${addZero(dateNow.getSeconds())}`);
+}
+
+// setInterval(timer, 1000)
+
+const regEmail = /^[a-zA-Z0-9_\.-]{2,}@[a-z0-9_]+\.?[a-z0-9_]+\.[a-z]{1,12}$/g;
+
+const user1 = {
+  email: '',
+  photoURL: '',
+  name: '',
+  registrationDate: '',
+  logOut: function () {
+    console.log(this);
+    
+    this.email = '';
+    this.name = '';
+    this.photoURL = '';
+    this.registrationDate = '';
+  },
+  test: function () {
+    console.log(this);
+    
+  }
+}
+
+const user2 = {
+  email: 'lkadshfglkdf@asdfasdf.com',
+  photoURL: 'asdfjhdsafsadf',
+  name: 'dkhfglkdfs',
+  registrationDate: '',
+  logOut: function () {
+    console.log(this);
+    
+    this.email = '';
+    this.name = '';
+    this.photoURL = '';
+    this.registrationDate = '';
+  },
+  test: () => console.log(this)
+}
+
+// console.log(this);
+// user1.test()
+// user2.test()
+
+const User = function (name, email, phone, private) {
+  this._name = name;
+  this._email = email;
+  this._phone = phone;
+  this.getName = () => this._name;
+  this.getPrivate = () => private;
+  this.setPrivate = (value) => {
+    const valReg = /[А-ЯЁа-яё]+/g
+    if (!valReg.test(value)) {
+      private = value;
+    } else {
+      throw new SyntaxError('Неправильные формат данных')
     }
   }
-
-  return count + 1
-}
-
-const count = strCount(str)
-console.log(count);
-
-/**
- * @param {string} string
- * @returns {string}
- */
-const firstLetterUpper = (string) => {
-  const lineArr = string.split('\n')
-  const stringArr = lineArr.map(function (line) {
-    // const strArr = line.split(' ')
-    // for (let i = 0; i < strArr.length; i++) {
-    //   strArr[i] = strArr[i][0].toUpperCase() + strArr[i].slice(1)
-    // }
-    // return strArr.join(' ')
-    return line
-      .split(' ')
-      .map((item) => item[0].toUpperCase() + item.slice(1))
-      .join(' ')
-  })
-
-  return stringArr.join('\n')
-}
-
-console.log(firstLetterUpper(str));
-
-
-// let reg = /\s+/
-console.log(('                   ajhs           dgfjk          hgdas          fjkas                        ').trim().replace(/\s+/, ' '));
-console.log(('                   ajhs           dgfjk          hgdas          fjkas                        ').trim().replaceAll(/\s+/gi, ' '));
-
-console.log(`sdhfghdfsg
-sdfgkldhsfgkj
-dsfjglfdg
-sdfgjlfds`)
-
-const pyramid = (h) => {
-  for (let i = 1; i <= h; i++) {
-    // document.write('<pre style="margin: 0">' + (' ').repeat(h - i) + ('^').repeat(i * 2 - 1) + '</pre>')
-    document.write(`<pre style='margin: 0;'>${(' ').repeat(h - i)}${('^').repeat(i * 2 - 1)}</pre>`)
+  // const self = this
+  // function convertName () {
+  //   console.log(self);
+     
+  //   return self._name.toUpperCase()
+  // }
+  const convertName = () => {
+    console.log(this);
+     
+    return this._name.toUpperCase()
   }
+  // this.getConvertName = function () {
+  //   return convertName()
+  // }
+  this.getConvertName = () => {
+    return convertName()
+  }
+  this.showName = () => console.log(this._name);
+  
 }
 
-pyramid(10);
+const user3 = new User('Alex', 'alex@mail.com', '3874568345', 'private')
+console.log(user3.getConvertName().split(''));
 
-let s2 = '1234';
+// function Student (name, email, phone, group) {
+//   User.call(this, name, email, phone)
+//   this.group = group
+//   this.getName = () => this._name.toUpperCase()
+// }
 
-const s3 = [].map.call(s2, function (item) {
-  return item ** 2
-})
-console.log(s3);
-
-const s4 = [].map.bind(s2, function (item) {
-  return item ** 3
-})
-
-console.log(s4());
+// const student = new Student('Alex', 'alex@mail.com', '3874568345', 'private')
+// console.log(student.getName());
