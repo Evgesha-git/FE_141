7890bb45162fe726cc2aef80158367ccbb027af1
Evgeny Kryzhu <e.kryzhu@colynn.pro>
2025-02-13 21:31:28 +0300

    ToDo list save localStorage

--- scripts/script.js
+++ scripts/script.js
@@ -1,144 +1,164 @@
-/** @type {HTMLElement} */
-const square = document.querySelector('.square');
-const time = getComputedStyle(square);
-console.log(time.transitionDuration);
+/**
+ * @todo Дома доработать таким образом, что бы при двайном клике по задаче, или при клике правой кнопкой мыши появлялась возможность редактирования, 
+ * а при нажатии Alt + Enter результат сохранялся
+ */
+/**
+ * @typedef {Object} TypeToDoItemParams
+ * @property {number} id
+ * @property {string} text
+ * @property {boolean} complite
+ */
 
-const getRandomColor = () => {
-  return Math.floor(Math.random() * 256)
+/**
+ * @param {TypeToDoItemParams} content 
+ */
+function ToDoItem(content) {
+  this.text = content.text
+  this.id = content.id
+  this.complite = content.complite
+
+  this.setComplite = () => {
+    this.complite = !this.complite
+  }
+
+  this.edit = (newValue) => {
+    this.text = newValue
+  }
 }
-
-// square.onclick = (event) => {
-//   square.style.transform = `rotate(${event.timeStamp}deg)`
-// }
-
-// square.onclick = () => {
-//   square.style.background = `rgb(${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()})`
-// }
-
-const rotateSqare = (event) => {
-  square.style.transform = `rotate(${event.timeStamp}deg)`
-}
-
-const setRandomColor = () => {
-  square.style.background = `rgb(${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()})`
-}
-
-square.addEventListener('click', rotateSqare)
-
-square.addEventListener('click', setRandomColor)
-
-// square.removeEventListener('click', setRandomColor)
-
-square.addEventListener('transitionstart', () => {
-  square.removeEventListener('click', rotateSqare)
-  square.removeEventListener('click', setRandomColor)
-})
-
-square.addEventListener('transitionend', () => {
-  square.addEventListener('click', rotateSqare)
-  square.addEventListener('click', setRandomColor)
-})
 
 /**
  * 
- * @param {string} buttonSelector селектор кнопки
- * @param {string} menuSelector селектор меню
- * @returns 
+ * @param {string} formSelector селектор формы ввода
+ * @param {string} inputSelector селектор текстового поля
+ * @param {string} listSelector селектор блока вывода
  */
-const menuOpenener = (buttonSelector, menuSelector) => {
-  /** @type {HTMLElement | null} */
-  const button = document.querySelector(buttonSelector);
-  /** @type {HTMLElement | null} */
-  const menu = document.querySelector(menuSelector);
-  let openFlag = false;
+const toListCreator = (formSelector, inputSelector, listSelector) => {
+  const form = document.querySelector(formSelector)
+  const input = document.querySelector(inputSelector)
+  const listContainer = document.querySelector(listSelector)
 
-  if (!button && !menu) return
+  if (!form && !input && !listContainer) return
 
-  const menuToggle = () => {
-    console.log(menu.classList.contains('active'))
-    if (!openFlag) { // menu.classList.contains('active')
-      menu.classList.add('active');
-      button.innerText = 'Close';
-      openFlag = !openFlag;
-    } else {
-      menu.classList.remove('active');
-      button.innerText = 'Open';
-      openFlag = !openFlag;
+  let todoListArr = []
+
+  const localsave = () => {
+    const jsonTodoListArr = JSON.stringify(todoListArr)
+    localStorage.setItem('todoList', jsonTodoListArr)
+  }
+
+  const render = () => {
+    listContainer.innerHTML = ''
+    todoListArr.forEach(todoListItem => {
+      const todolistLine = document.createElement('div')
+      todolistLine.classList.add('todo-list--item')
+
+      const todoCheckBox = document.createElement('input')
+      todoCheckBox.setAttribute('type', 'checkbox')
+      todoCheckBox.classList.add('todo-list--item-check')
+      todoCheckBox.checked = todoListItem.complite
+
+      todoCheckBox.addEventListener('click', (event) => {
+        event.preventDefault()
+        todoListItem.setComplite()
+        render()
+        localsave()
+      })
+
+      const todoText = document.createElement('p')
+      todoText.classList.add('todo-list--item-text')
+      if (todoListItem.complite) {
+        todoText.classList.add('todo-list--item-text__active')
+      }
+      todoText.innerText = todoListItem.text
+
+      todoText.addEventListener('click', () => {
+        todoListItem.setComplite()
+        render()
+        localsave()
+      })
+
+      const delButton = document.createElement('button')
+      delButton.classList.add('todo-list--item-button')
+      delButton.innerText = 'Del'
+      delButton.addEventListener('click', () => {
+        if (confirm('Вы точно хотите удалить?')) {
+          todoListArr = todoListArr.filter(item => item.id !== todoListItem.id)
+          render()
+          localsave()
+        }
+      })
+
+      todolistLine.append(todoCheckBox, todoText, delButton)
+      listContainer.append(todolistLine)
+    })
+  }
+
+  if (localStorage.getItem('todoList')) {
+    const todoListStore = localStorage.getItem('todoList')
+    /** @type {TypeToDoItemParams[]} */
+    const todoListStoreData = JSON.parse(todoListStore)
+
+    todoListStoreData.forEach(item => {
+      const todoListItem = new ToDoItem(item)
+      todoListArr.push(todoListItem)
+    })
+    render()
+  }
+
+  const groupTodoItemsById = Object.groupBy(todoListArr, ({ id }) => id)
+
+  const getId = () => {
+    const id = Math.floor(Math.random() * 10000)
+    if (Object.keys(groupTodoItemsById).length >= 9999) return
+    if (Object.keys(groupTodoItemsById).includes('' + id)) {
+      return getId()
     }
+    return id
   }
 
-  button.addEventListener('click', menuToggle)
-  menu.addEventListener('transitionstart', () => {
-    button.removeEventListener('click', menuToggle)
-  })
-  menu.addEventListener('transitionend', () => {
-    button.addEventListener('click', menuToggle)
-  })
+  const todoCreate = (event) => {
+    event.preventDefault()
+    const text = input.value
+
+    if (!text.length) return
+
+    /** @type {TypeToDoItemParams} */
+    const data = {
+      complite: false,
+      text,
+      id: getId()
+    }
+
+    const todoItem = new ToDoItem(data)
+    todoListArr.push(todoItem)
+    render()
+    localsave()
+  }
+
+  form.addEventListener('submit', todoCreate)
 }
 
-menuOpenener('.menuToggle', '.menu')
+toListCreator('.todo-form', '#todo-text', '.todo-list')
 
-/**
- * 
- * @param {string} textSelector селектор элемента с текстом
- */
-const textEdit = (textSelector) => {
-  const textElements = document.querySelectorAll(textSelector)
-
-  /**
-   * 
-   * @param {HTMLElement} textElement 
-   */
-  const textContentEdit = (textElement) => {
-    textElement.addEventListener('dblclick', function () {
-      console.log(this);
-      this.contentEditable = true
-    })
-
-    textElement.addEventListener('click', (event) => {
-      /** @type {HTMLElement} */
-      const target = event.target
-      console.log(event);
-      
-      if (target.nodeName === 'SPAN') {
-        target.innerHTML += '!'
-      }
-      if (target.nodeName === 'P') {
-        target.innerHTML = '!' + target.innerHTML
-      }
-    }, true)
-
-    textElement.addEventListener('keydown', function (event) {
-      console.log(event);
-      
-      if (event.altKey && event.key === 'Enter') {
-        this.contentEditable = false
-      }
-    })
+const obj = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: {
+    a1: 4,
+    b1: 5,
+    c1: 6
   }
-
-  textElements.forEach(textContentEdit)
 }
 
-textEdit('#text')
+const { b: x, c, a, d: { a1, b1, c1 } } = obj
 
-const listItem = document.createElement('div')
-const contentText = document.createElement('p')
+console.log(obj);
+console.log(a);
+console.log(x);
+console.log(c);
+console.log(a1);
+console.log(b1);
+console.log(c1);
 
-contentText.innerText = 'text'
-contentText.addEventListener('dblclick', function () {
-  this.contentEditable = true
-})
 
-contentText.addEventListener('keydown', function (event) {
-  if (event.altKey && event.key === 'Enter') {
-    this.contentEditable = false
-  }
-})
-
-listItem.append(contentText);
-document.body.append(listItem);
-
-localStorage.setItem('test', 'dfkghkdfj')
-
-console.log(JSON.parse(localStorage.getItem('notes')));
