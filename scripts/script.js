3ae9ce0ce614a657372427529a6a2b1f7974e5cf
Evgeny Kryzhu <e.kryzhu@colynn.pro>
2025-03-03 21:45:11 +0300

    add calculator

--- scripts/script.js
+++ scripts/script.js
@@ -1,130 +1,157 @@
-const accordeon = (accordeonSelector) => {
-  const accordeonContainers = document.querySelectorAll(accordeonSelector)
+const calculateCreator = (calculateSelector) => {
+  const calculateContainer = document.querySelector(calculateSelector)
+  if (!calculateContainer) return
+  /** @type {HTMLElement} */
+  const input = calculateContainer.querySelector('.calculate__input')
+  /** @type {HTMLElement} */
+  const buttons = calculateContainer.querySelector('.calculate__buttons')
+  const calculate = {
+    value: '',
+    operand: null,
+    waitingSecondOperand: false,
+    operator: null
+  }
+
+  const formaterNumber = new Intl.NumberFormat('ru-RU', {
+    maximumFractionDigits: 100
+  })
+
+  const operations = {
+    '/': (firs, second) => ((firs * 10) / (second * 10)) / 10,
+    '*': (firs, second) => ((firs * 10) * (second * 10)) / 10,
+    '-': (firs, second) => ((firs * 10) - (second * 10)) / 10,
+    '+': (firs, second) => ((firs * 10) + (second * 10)) / 10
+  }
 
   /**
    * 
-   * @param {HTMLDivElement} container 
+   * @param {string} value 
    */
-  const accordeonHandler = (container) => {
-    const titels = container.querySelectorAll('.accordeon__title')
+  const render = (value) => {
+    if (value[value.length - 1] === '.') {
+      input.innerText = formaterNumber.format(+value) + ','
+    } else {
+      input.innerText = formaterNumber.format(+value)
+    }
+    console.log(formaterNumber.format(+value));
+    
+  }
 
-    /**
-     * 
-     * @param {HTMLElement} title 
-     */
-    const titleHandler = (title) => {
-      title.addEventListener('click', () => {
-        title.classList.toggle('active')
-        titels.forEach(item => {
-          if (item !== title) {
-            item.classList.remove('active')
-          }
-        })
-      })
+  const formatedCurrent = (value) => {
+    const trimVal = value.replaceAll(/\s/g, '')
+    return trimVal.replaceAll(',', '.')
+  }
+
+  const clear = () => {
+    calculate.operand = null
+    calculate.value = ''
+    calculate.waitingSecondOperand = false
+    calculate.operator = null
+    input.innerText = 0
+  }
+
+  const setNumber = (value) => {
+    const currentValue = formatedCurrent(input.innerText)
+
+
+    if (calculate.waitingSecondOperand) {
+      if (value === '.') {
+        if (currentValue.indexOf('.') !== -1) return
+        calculate.value = 0 + value
+      } else {
+        calculate.value = value
+      }
+      calculate.waitingSecondOperand = false
+    } else if (calculate.operator === '=') {
+      if (value === '.') {
+        if (currentValue.indexOf('.') !== -1) return
+        calculate.value = 0 + value
+      } else {
+        calculate.value = value
+      }
+      calculate.operator = null
+    } else {
+      if (value === '.') {
+        if (currentValue.indexOf('.') !== -1) return
+        calculate.value = currentValue + value
+      } else {
+        calculate.value = +(currentValue + value)
+      }
     }
 
-    titels.forEach(titleHandler)
+
+    render(calculate.value)
   }
 
-  accordeonContainers.forEach(accordeonHandler)
-}
-
-accordeon('.accordeon-container')
-
-function Test(test1, test2) {
-  this.test1 = test1
-  this.test2 = test2
-}
-
-const test1 = new Test(1, 2)
-const test2 = new Test(1, 2)
-const test3 = test2
-
-const calculate = () => {
-  const container = document.querySelector('.numbers')
-  const input = document.querySelector('.numbers__input')
-  const math = {
-    operandA: null,
-    result: null
+  const percent = (current) => {
+    calculate.value = calculate.operand / 100 * +current
   }
 
-  /**
-   * 
-   * @param {MouseEvent} event 
-   */
-  const clickHandler = (event) => {
+  const abs = () => {
+    calculate.value = -(+calculate.value)
+  }
+
+  const handleOperators = (operator, current) => {
+    if (calculate.waitingSecondOperand) {
+      const val = operations[calculate.operator](calculate.operand, +current)
+      calculate.value = val
+      calculate.operand = val
+    } else {
+      calculate.operator = operator
+      calculate.waitingSecondOperand = true
+      calculate.operand = calculate.value
+      calculate.value = 0
+    }
+  }
+
+  const handleResult = (operator, current) => {
+    const val = operations[calculate.operator](calculate.operand, +current)
+    calculate.value = val
+    calculate.waitingSecondOperand = false
+    calculate.operand = null
+    calculate.operator = operator
+  }
+
+  const setOperator = (operator) => {
+    const currentValue = formatedCurrent(input.innerText)
+
+    if (operator !== '=') {
+      if (operator === '+/-') {
+        abs()
+      } else if (operator === '%') {
+        percent(currentValue)
+      } else {
+        handleOperators(operator, currentValue)
+      }
+    } else {
+      handleResult(operator, currentValue)
+    }
+
+    render(calculate.value)
+  }
+
+  buttons.addEventListener('click', (e) => {
     /** @type {HTMLElement} */
-    const target = event.target
-    console.log(target.className);
+    const target = e.target
 
-    switch (target.className) {
-      case 'numbers__button':
-        let value = input.value
-        const num = target.dataset.value
-        input.value = value + num
-        return
-      case 'numbers__buttons':
-        if (!math.operandA) {
-          math.operandA = +input.value
-          input.value = ''
-        } else {
-          const num = +input.value
-          math.result = math.operandA + num
-          math.operandA = math.result
-          input.value = ''
-        }
-        return
-      case 'numbers__input':
-        if (!math.result) {
-          input.value = math.operandA + +input.value
-        } else {
-          input.value = math.result + +input.value
-          math.operandA = null
-          math.result = null
-        }
-        return
-      default:
-        input.value = ''
-        math.operandA = null
-        math.result = null
+    if (!target.classList.contains('button')) return
+
+    if (target.classList.contains('clear')) {
+      clear()
     }
 
-    // if (target.classList.contains('numbers__button')) {
-    //   let value = input.value
-    //   const num = target.dataset.value
-    //   input.value = value + num
-    // }
+    if (target.classList.contains('number')) {
+      const value = target.dataset.value
+      if (!value) return
+      setNumber(value)
+    }
 
-    // if (target.classList.contains('numbers__buttons')) {
-    //   if (!math.operandA) {
-    //     math.operandA = +input.value
-    //     input.value = ''
-    //   } else {
-    //     const num = +input.value
-    //     math.result = math.operandA + num
-    //     math.operandA = math.result
-    //     input.value = ''
-    //   }
-    // }
-
-    // if (target.classList.contains('numbers__input')) {
-    //   if (!math.result) {
-    //     input.value = math.operandA + +input.value
-    //   } else {
-    //     input.value = math.result + +input.value
-    //     math.operandA = null
-    //     math.result = null
-    //   }
-    // }
-
-    // if (target.classList.contains('numbers')) {
-    //   input.value = ''
-    //   math.operandA = null
-    //   math.result = null
-    // }
-  }
-
-  container.addEventListener('click', clickHandler)
+    if (target.classList.contains('operator')) {
+      const operator = target.dataset.value
+      if (!operator) return
+      setOperator(operator)
+    }
+  })
 }
 
-calculate()
+calculateCreator('.calculate')
