4b5ee57fbf3536243184b8f594047e8570cd5210
Evgeny Kryzhu <e.kryzhu@colynn.pro>
2025-03-10 21:35:26 +0300

    Прототипное наследование

--- scripts/script.js
+++ scripts/script.js
@@ -1,85 +1,179 @@
-/**
- * @typedef {Object} ConverterTypes
- * @property {string} input 
- * @property {string} output
- * @property {string} button
- */
+function Class1(par1, par2) {
+  this.par1 = par1
+  this.par2 = par2
 
-/**
- * 
- * @param {ConverterTypes} param0 
- */
-const converter = ({ button, input, output }) => {
-  const inputElement = document.querySelector(input)
-  const outputElement = document.querySelector(output)
-  const btn = document.querySelector(button)
+  // this.method1 = () => {
+  //   return this.par1 + this.par2
+  // }
+}
 
-  if (!inputElement || !outputElement || !btn) return
+Class1.prototype.method1 = function () {
+  return this.par1 + this.par2
+}
 
-  const convert = () => {
-    /** @type {string} */
-    const value = inputElement.value
+function Class2(par1, par2, par3) {
+  Class1.apply(this, [par1, par2])
+  this.par3 = par3
+  // this.method1 = () => {
+  //   return this.par1 - this.par2
+  // }
+  // this.method2 = () => {
+  //   return this.method1() + this.par3
+  // }
 
-    const resultArr = [...value].map(item => {
-      const code = item.charCodeAt()
-      return code.toString(2)
+  this.method1 = Class1.prototype.method1.bind(this)
+
+  this.render = () => {
+    const button = document.createElement('button')
+    button.innerText = 'Нажми на меня'
+    button.addEventListener('click', () => {      
+      alert(this.method1())
     })
-
-    outputElement.value = resultArr.join('.')
+    document.body.append(button)
   }
 
-  btn.addEventListener('click', convert)
+  Object.defineProperties(this, {
+    status: {
+      enumerable: true,
+      configurable: true,
+      set: (val) => {
+        if (typeof val !== 'number') {
+          throw new TypeError('Invalid type')
+        }
+        status = val
+      },
+      get: () => +status
+    }
+  })
 }
 
-converter({ button: '#button', input: '#input', output: '#output' })
+Class2.prototype = Object.create(Class1.prototype)
+
+const test0 = new Class1(1, 2)
+const test = new Class2(2, 4, 6)
+test.render()
 
 /**
  * 
- * @param {ConverterTypes} param0 
+ * @param {{title: string, content: string}} data 
  */
-const converter2 = ({ button, input, output}) => {
-  const inputElement = document.querySelector(input)
-  const outputElement = document.querySelector(output)
-  const btn = document.querySelector(button)
-
-  if (!inputElement || !outputElement || !btn) return
-
-  const convert = () => {
-    /** @type {string} */
-    const value = inputElement.value
-
-    if (!value.length) return
-
-    const valueArr = value.split('.')
-
-    const resultArr = valueArr.map(item => {
-      const code = parseInt(item, 2)
-      return String.fromCharCode(code)
-    })
-    
-    outputElement.value = resultArr.join('')
-  }
-
-  btn.addEventListener('click', convert)
+function Note(data) {
+  this.data = data
 }
 
-converter2({ button: '#buttonRev', input: '#inputRev', output: '#outputRev' })
+Note.prototype.edit = function (newData) {
+  // this.data = Object.assign(this.data, newData)
+  this.data = { ...this.data, ...newData }
+}
 
-const create = (tagName) => {
-  const elem = document.createElement(tagName)
-  return elem
+const note0 = new Note({ title: 'Заметка 0', content: 'Контент 0' })
+
+const obj1 = {}
+
+Object.defineProperty(obj1, 'name', {
+  // value: 10,
+  // writable: false,
+  enumerable: true,
+  set: (val) => {
+    if (typeof val !== 'number') {
+      throw new TypeError('Неверный тип данных')
+    }
+    name = val
+  },
+  get: () => name
+})
+
+const f1 = function () {
+  console.log(this);
+  
+  return this.a + this.b
+}
+
+const obj2 = {
+  a: 1,
+  b: 2,
+  m1: f1
+}
+
+const obj3 = {
+  a: 10,
+  b: 20,
+  m1: f1.bind(obj2)
+}
+
+const str = 'Lorem ipsum dolor sit amet.'
+
+// const arrChar = Array.prototype.map.call(str, (item) => item.toUpperCase())
+// console.log(arrChar);
+
+String.prototype.map = function (callback) {
+  return Array.prototype.map.call(this, callback)
+}
+
+// const getArrChar = Array.prototype.map.bind(str)
+
+// console.log(getArrChar((item) => item.toUpperCase()));
+
+console.log(str.map((item) => item.toUpperCase()));
+
+const inventory = [
+  { name: "asparagus", type: "vegetables", quantity: 5 },
+  { name: "bananas", type: "fruit", quantity: 0 },
+  { name: "goat", type: "meat", quantity: 23 },
+  { name: "cherries", type: "fruit", quantity: 5 },
+  { name: "fish", type: "meat", quantity: 22 },
+];
+
+
+if (!Object.groupBy) {
+  Object.prototype.groupBy = function (arr, callback) {
+    return arr.reduce((acc = {}, ...args) => {
+      const key = callback(...args);
+      acc[key] = acc[key] ?? []
+      acc[key].push(args[0]);
+      return acc;
+    }, {})
+  }
+}
+
+const groupInventory = Object.groupBy(inventory, (item) => item.name)
+
+console.log(groupInventory);
+
+function Dz7Constructor(params) {
+  this.params = params
+}
+
+function ConstructorDOM (params) {
+  Dz7Constructor.call(this, params)
+
+  this.create = (tagName) => {
+    return document.createElement(tagName)
+  }
 }
 
 /**
  * 
- * @param {HTMLElement} elem 
+ * @param {HTMLElement} element 
  * @param {string} name 
- * @param {?string} value 
+ * @param {string|boolean} value 
+ * @returns 
  */
-const attr = (elem, name, value) => {
-  if (value) {
-    elem.setAttribute(name, value)
+ConstructorDOM.prototype.attr = function (element, name, value) {
+  if (!value && typeof value !== 'boolean') {
+    return element.getAttribute(name)
   } else {
-    return elem.getAttribute(name)
+    return element.setAttribute(name, value)
   }
 }
+
+const o1 = {
+  name: 'Alex',
+  age: 55
+}
+
+const o2 = {
+  cours: 3
+}
+
+o2.__proto__ = o1
