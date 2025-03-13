1f0672b052a6201c2669a1146efb889bb24c7906
Evgeny Kryzhu <e.kryzhu@colynn.pro>
2025-03-13 21:49:39 +0300

    classes

--- scripts/script.js
+++ scripts/script.js
@@ -1,179 +1,201 @@
-function Class1(par1, par2) {
-  this.par1 = par1
-  this.par2 = par2
+class Test {
+  #a = null
+  #b = null
 
-  // this.method1 = () => {
-  //   return this.par1 + this.par2
-  // }
-}
-
-Class1.prototype.method1 = function () {
-  return this.par1 + this.par2
-}
-
-function Class2(par1, par2, par3) {
-  Class1.apply(this, [par1, par2])
-  this.par3 = par3
-  // this.method1 = () => {
-  //   return this.par1 - this.par2
-  // }
-  // this.method2 = () => {
-  //   return this.method1() + this.par3
-  // }
-
-  this.method1 = Class1.prototype.method1.bind(this)
-
-  this.render = () => {
-    const button = document.createElement('button')
-    button.innerText = 'Нажми на меня'
-    button.addEventListener('click', () => {      
-      alert(this.method1())
-    })
-    document.body.append(button)
+  constructor(a, b, c) {
+    this.#a = a
+    this.#b = b
+    this.c = c
   }
 
-  Object.defineProperties(this, {
-    status: {
-      enumerable: true,
-      configurable: true,
-      set: (val) => {
-        if (typeof val !== 'number') {
-          throw new TypeError('Invalid type')
-        }
-        status = val
-      },
-      get: () => +status
-    }
-  })
+  getA() {
+    return this.#a
+  }
+
+  setA(val) {
+    this.#a = val
+  }
+
+  get b() {
+    return this.#b
+  }
+
+  set b(val) {
+    this.#b = val
+  }
 }
 
-Class2.prototype = Object.create(Class1.prototype)
-
-const test0 = new Class1(1, 2)
-const test = new Class2(2, 4, 6)
-test.render()
+const test = new Test(1, 2, 3)
 
 /**
- * 
- * @param {{title: string, content: string}} data 
+ * @typedef {Object} NoteType
+ * @property {string} id
+ * @property {string} title
+ * @property {string} content
  */
-function Note(data) {
-  this.data = data
-}
 
-Note.prototype.edit = function (newData) {
-  // this.data = Object.assign(this.data, newData)
-  this.data = { ...this.data, ...newData }
-}
+class Note {
+  /** @type {NoteType} */
+  data = {}
 
-const note0 = new Note({ title: 'Заметка 0', content: 'Контент 0' })
-
-const obj1 = {}
-
-Object.defineProperty(obj1, 'name', {
-  // value: 10,
-  // writable: false,
-  enumerable: true,
-  set: (val) => {
-    if (typeof val !== 'number') {
-      throw new TypeError('Неверный тип данных')
+  /**
+   * @param {{title: ?string, content: ?string}} param0 
+   */
+  constructor({ title, content }) {
+    if (!title && !content)
+      throw new Error('Хотя бы одно поле должно быть заполено')
+    this.data = {
+      content,
+      title
     }
-    name = val
-  },
-  get: () => name
-})
+  }
 
-const f1 = function () {
-  console.log(this);
-  
-  return this.a + this.b
+  /**
+   * 
+   * @param {NoteType} data 
+   */
+  edit(data) {
+    this.data = Object.assign(this.data, data)
+    // this.data = {...this.data, ...data}
+  }
+
+  get() {
+    return this.data
+  }
 }
 
-const obj2 = {
-  a: 1,
-  b: 2,
-  m1: f1
-}
+class Notes {
+  /** @type {{data: NoteType}[]} */
+  notes = []
 
-const obj3 = {
-  a: 10,
-  b: 20,
-  m1: f1.bind(obj2)
-}
+  add({ title, content }) {
+    try {
+      const note = new Note({ title, content })
+      const id = Date.now().toString(36)
+      note.edit({ id })
 
-const str = 'Lorem ipsum dolor sit amet.'
+      this.notes.push(note)
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
-// const arrChar = Array.prototype.map.call(str, (item) => item.toUpperCase())
-// console.log(arrChar);
-
-String.prototype.map = function (callback) {
-  return Array.prototype.map.call(this, callback)
-}
-
-// const getArrChar = Array.prototype.map.bind(str)
-
-// console.log(getArrChar((item) => item.toUpperCase()));
-
-console.log(str.map((item) => item.toUpperCase()));
-
-const inventory = [
-  { name: "asparagus", type: "vegetables", quantity: 5 },
-  { name: "bananas", type: "fruit", quantity: 0 },
-  { name: "goat", type: "meat", quantity: 23 },
-  { name: "cherries", type: "fruit", quantity: 5 },
-  { name: "fish", type: "meat", quantity: 22 },
-];
-
-
-if (!Object.groupBy) {
-  Object.prototype.groupBy = function (arr, callback) {
-    return arr.reduce((acc = {}, ...args) => {
-      const key = callback(...args);
-      acc[key] = acc[key] ?? []
-      acc[key].push(args[0]);
-      return acc;
+  get noteById () {
+    return this.notes.reduce((acc, note) => {
+      acc[note.data.id] = note
+      return acc
     }, {})
   }
-}
 
-const groupInventory = Object.groupBy(inventory, (item) => item.name)
+  edit (id, newData) {
+    const note = this.noteById[id]
+    if (!note) return
+    note.edit(newData)
+  }
 
-console.log(groupInventory);
-
-function Dz7Constructor(params) {
-  this.params = params
-}
-
-function ConstructorDOM (params) {
-  Dz7Constructor.call(this, params)
-
-  this.create = (tagName) => {
-    return document.createElement(tagName)
+  remove (id) {
+    this.notes = this.notes.filter(note => note.data.id !== id)
   }
 }
 
-/**
- * 
- * @param {HTMLElement} element 
- * @param {string} name 
- * @param {string|boolean} value 
- * @returns 
- */
-ConstructorDOM.prototype.attr = function (element, name, value) {
-  if (!value && typeof value !== 'boolean') {
-    return element.getAttribute(name)
-  } else {
-    return element.setAttribute(name, value)
+// const notes = new Notes()
+
+class NotesApp extends Notes {
+  constructor (selector) {
+    super()
+    this.container = document.querySelector(selector)
+    this.noteContainer = document.createElement('div')
+    this.init()
+  }
+
+  init () {
+    const form = document.createElement('form')
+    const title = document.createElement('input')
+    title.setAttribute('type', 'text')
+    const content = document.createElement('textarea')
+    const submit = document.createElement('button')
+    submit.setAttribute('type', 'submit')
+    submit.innerText = 'Add'
+
+    form.append(title, content, submit)
+    form.addEventListener('submit', (event) => {
+      event.preventDefault()
+      const data = {
+        title: title.value,
+        content: content.value
+      }
+
+      this.add(data)
+      this.render()
+
+      title.value = ''
+      content.value = ''
+    })
+
+    this.noteContainer.classList.add('notes')
+
+    this.container.append(form, this.noteContainer)
+    this.render()
+  }
+
+  render () {
+    if (!this.notes.length) { //this.notes.length === 0
+      this.noteContainer.innerHTML = `<h2 className="title">Список заметок пуст</h2>`
+    } else {
+      this.noteContainer.innerHTML = ''
+      this.notes.forEach(note => {
+        let editable = false
+        const noteItem = document.createElement('div')
+        noteItem.classList.add('item')
+        const contents = document.createElement('div')
+        contents.classList.add('noteContent')
+        const title = document.createElement('h2')
+        const content = document.createElement('p')
+        const remove = document.createElement('button')
+        const edit = document.createElement('button')
+
+        remove.classList.add('remove')
+        remove.innerHTML = '&#10060;'
+        remove.addEventListener('click', () => {
+          if (confirm('Вы точно хотите удалить?')) {
+            this.remove(note.data.id)
+            this.render()
+          }
+        })
+
+        edit.classList.add('edit')
+        edit.innerHTML = '&#9998;'
+        edit.addEventListener('click', () => {
+          if (editable) {
+            edit.innerHTML = '&#9998;'
+            title.contentEditable = false
+            content.contentEditable = false
+            const data = {
+              title: title.innerText,
+              content: content.innerText
+            }
+
+            this.edit(note.data.id, data)
+            this.render()
+            editable = !editable
+          } else {
+            edit.innerHTML = '&#128428;'
+            title.contentEditable = true
+            content.contentEditable = true
+            editable = !editable
+          }
+        })
+
+        title.innerText = note.data.title
+        content.innerText = note.data.content
+
+        contents.append(title, content)
+        noteItem.append(contents, edit, remove)
+
+        this.noteContainer.append(noteItem)
+      })
+    }
   }
 }
 
-const o1 = {
-  name: 'Alex',
-  age: 55
-}
-
-const o2 = {
-  cours: 3
-}
-
-o2.__proto__ = o1
+new NotesApp('.container')
